251c1452f81b708e6e648ef31484f3e1ce8b14cf
ChepJM <chepurkojm@gmail.com>
2023-01-12 07:24:08 +0900

    Add solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld firstHelloWorldBean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld secondHelloWorldBean = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat firstCatBean = applicationContext.getBean("cat", Cat.class);
+        Cat secondCatBean = applicationContext.getBean("cat", Cat.class);
+        System.out.println(firstHelloWorldBean.getMessage());
+
+        System.out.println("Are firstHelloWorldBean and secondHelloWorldBean same objects? - "
+                + (firstHelloWorldBean == secondHelloWorldBean));
+        System.out.println("Are firstCatBean and secondCatBean same objects? - "
+                + (firstCatBean == secondCatBean));
+        System.out.println();
     }
 }
